05dc969b2ba595aaf7f9098673013d823681a7ff
teskgro <62964567+teskgro@users.noreply.github.com>
2026-07-16 00:09:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const imageCount = 38;
+const folder = "assets";
+const extension = "jpg";
+const images = Array.from({ length: imageCount }, (_, i) => i + 1);
+
+// Shuffle
+
+images.sort(() => Math.random() - 0.5);
+images.forEach((image, index) => {
+    const layer = document.createElement("div");
+    layer.className = "a";
+    layer.id = `a${index + 1}`;
+    layer.style.backgroundImage = `url("${folder}/${image}.${extension}")`;
+    document.querySelector(".i").before(layer);
+});
+
 var divs = document.querySelectorAll('.a');
 var i = Math.floor(Math.random() * divs.length);
 
